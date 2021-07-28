6ccdaac778840f5ea1c7472c198f28e4d0c6de83
Zack Cerza <zack@redhat.com>
2021-07-28 09:04:08 -0600

    index: Dark mode

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import axios from "axios";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ReactQueryDevtools } from "react-query/devtools";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import "./index.css";
 import App from "./App";
@@ -25,18 +27,54 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-      </QueryClientProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function useDarkMode() {
+  const systemDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [state, setState] = React.useState({ system: systemDarkMode });
+
+  function setDarkMode(value) {
+    const newState = { ...state, user: value };
+    if (value !== state.user) {
+      setState(newState);
+    }
+    setState(newState);
+  }
+  const darkMode = state.user === undefined ? systemDarkMode : state.user;
+  return [darkMode, setDarkMode];
+}
+
+export default function Root() {
+  const [darkMode, setDarkMode] = useDarkMode();
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  const theme = React.useMemo(() => {
+    const paletteType = darkMode ? "dark" : "light";
+    const theme = createTheme({ palette: { type: paletteType } });
+    if (darkMode) {
+      theme.palette.background.default = "#181818";
+      theme.palette.background.paper = "#303030";
+    }
+    return theme;
+  }, [darkMode]);
+  if (darkMode === undefined) {
+    return null;
+  }
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <App />
+          </QueryClientProvider>
+        </Router>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
