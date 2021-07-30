c9aaf1f1959fdebd1c3c62a7197fed18286972a8
Zack Cerza <zack@redhat.com>
2021-07-30 17:35:48 -0600

    FilterMenu: Greatly improve option display
    As you type, you'll now see options listed in this order:
    
    1. An exact match, if any
    2. Options that start with the input value
    3. Options that contain the input value
    
    This is intended to make it easier to locate the desired option.

--- src/components/FilterMenu/index.js
+++ src/components/FilterMenu/index.js
@@ -46,10 +46,31 @@ export default function FilterMenu(props) {
   const onChange = (_, value) => {
     props.dispatch({ type: "set", payload: { key: props.type, value } });
   };
+  const filterOptions = (options, { inputValue }) => {
+    if (!inputValue) return options;
+    let result = [];
+    result.push(...options.filter((item) => item === inputValue));
+    result.push(
+      ...options.filter((item) => {
+        if (result.indexOf(item) !== -1) return false;
+        return item.startsWith(inputValue);
+      })
+    );
+    result.push(
+      ...options.filter((item) => {
+        if (result.indexOf(item) !== -1) return false;
+        return item.includes(inputValue);
+      })
+    );
+    return result;
+  };
 
   return (
     <Autocomplete
       value={props.value}
+      filterOptions={filterOptions}
+      autoHighlight
+      autoSelect
       loading={query.isLoading}
       onChange={onChange}
       options={query.data || []}
