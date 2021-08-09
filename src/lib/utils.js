80e765451a47925ebc1c5c19d970cb027192ba05
Zack Cerza <zack@redhat.com>
2021-08-09 13:54:04 -0600

    utils: Add getDuration

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,6 +9,19 @@ function pad(num) {
   return `${num}`.padStart(2, "0");
 }
 
+function getDuration(seconds) {
+  const result = {};
+  let seconds_ = seconds;
+  result.days = Math.floor(seconds_ / (60 * 60 * 24));
+  seconds_ %= 60 * 60 * 24;
+  result.hours = Math.floor(seconds_ / (60 * 60));
+  seconds_ %= 60 * 60;
+  result.minutes = Math.floor(seconds_ / 60);
+  seconds_ %= 60;
+  result.seconds = seconds_;
+  return result;
+}
+
 function formatDuration(seconds) {
   let result = seconds;
   const hours = Math.floor(result / 3600);
@@ -18,4 +31,4 @@ function formatDuration(seconds) {
   return `${pad(hours)}:${pad(minutes)}:${pad(result)}`;
 }
 
-export { formatDate, formatDuration };
+export { formatDate, getDuration, formatDuration };
