fad4fd4eb34beb340bf750a1f88eb0dd8a899930
Zack Cerza <zack@redhat.com>
2021-08-10 16:14:50 -0600

    Job: grid

--- src/pages/Job/index.js
+++ src/pages/Job/index.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import { useTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
@@ -42,39 +45,61 @@ function StatusIcon({ status }) {
 
 function JobHeader({ query }) {
   if (!query.isSuccess) return null;
-  const dateFields = ["posted", "started", "updated"];
-  const fields = [
-    "duration",
-    "branch",
-    "machine_type",
-    "teuthology_branch",
-    "os_type",
-    "os_version",
-  ];
   return (
     <>
-      <div>
-        Started {formatRelative(new Date(query.data.started), new Date())}
-        Started {formatDistanceToNow(new Date(query.data.started))} ago Started{" "}
-        {formatDistanceToNowStrict(new Date(query.data.started))} ago Took{" "}
-        {formatDuration(getDuration(query.data.duration))}
-      </div>
-      <div style={{ height: "auto", display: "flex" }}>
-        {dateFields.map((field) => {
-          return (
-            <Typography key={field}>
-              {field}: {formatDate(query.data[field])}
-            </Typography>
-          );
-        })}
-        {fields.map((field) => {
-          return (
-            <Typography key={field}>
-              {field}: {query.data[field]}
-            </Typography>
-          );
-        })}
-      </div>
+      <Grid item xs={4}>
+        <Typography>Status: {query.data.status}</Typography>
+        <Typography>
+          Started {formatDistanceToNow(new Date(query.data.started))} ago
+        </Typography>
+        {query.data.duration ? (
+          <Typography>
+            Took {formatDuration(getDuration(query.data.duration))}
+          </Typography>
+        ) : null}
+      </Grid>
+      <Grid item xs={4}>
+        <Typography>Ceph Branch: {query.data.branch}</Typography>
+        <Typography>
+          SHA1: <code>{query.data.sha1.slice(0, 7)}</code>
+        </Typography>
+        <Typography>
+          Teuthology Branch: {query.data.teuthology_branch}
+        </Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography>
+          Nodes:&nbsp;
+          {Object.keys(query.data.targets).map((item) => {
+            return (
+              <RouterLink
+                to={`/nodes/${item}`}
+                component={Link}
+                target="_blank"
+              >
+                {item.split(".")[0]}
+              </RouterLink>
+            );
+          })}
+        </Typography>
+        <Typography>
+          OS: {query.data.os_type} {query.data.os_version}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography component="span">Description:&nbsp;</Typography>
+        <Typography variant="body2" component="span">
+          <code>{query.data.description}</code>
+        </Typography>
+      </Grid>
+      {query.data.failure_reason ? (
+        <Grid item xs={12}>
+          <Typography component="span">Failure reason:&nbsp;</Typography>
+          <Typography variant="body2" component="span">
+            <code>{query.data.failure_reason}</code>
+          </Typography>
+        </Grid>
+      ) : null}
     </>
   );
 }
@@ -110,30 +135,30 @@ export default function Job() {
   const { name, job_id } = useParams();
   const query = useJob(name, job_id);
   return (
-    <div>
-      <div
-        style={{ display: "flex", margin: "20px", justifyContent: "center" }}
-      >
-        <StatusIcon
-          status={query.data?.status}
-          style={{ alignSelf: "center" }}
-        />
+    <Grid container spacing={2}>
+      <Grid item xs={12} style={{ display: "flex" }}>
+        <StatusIcon status={query.data?.status} />
         <Typography variant="h5">
-          {name}/{job_id}
+          <RouterLink to={`/runs/${name}`} component={Link} target="_blank">
+            {name}
+          </RouterLink>
+          /{job_id}
         </Typography>
-      </div>
+      </Grid>
       <JobHeader query={query} />
-      <Accordion
-        TransitionProps={{ unmountOnExit: true }}
-        style={{ margin: "20px" }}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>Full job details</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <JobDetails query={query} />
-        </AccordionDetails>
-      </Accordion>
-    </div>
+      <Grid item xs={12}>
+        <Accordion
+          TransitionProps={{ unmountOnExit: true }}
+          style={{ marginTop: "20px" }}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>Full job details</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <JobDetails query={query} />
+          </AccordionDetails>
+        </Accordion>
+      </Grid>
+    </Grid>
   );
 }
