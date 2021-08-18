c0b8ed9d9d1e0a81988608aceb3ecb38bfd1e7cb
Zack Cerza <zack@redhat.com>
2021-08-18 16:01:17 -0600

    Run: Add links to queries for similar runs

--- src/pages/Run/index.js
+++ src/pages/Run/index.js
@@ -4,13 +4,14 @@ import { makeStyles } from "@material-ui/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { format } from "date-fns";
 
 import { useRun } from "../../lib/paddles";
 import JobList from "../../components/JobList";
+import Link from "../../components/Link";
 
 const useStyles = makeStyles(() => ({
   root: {
-    fontSize: 12,
     "& .MuiButton-root": {
       textTransform: "none",
     },
@@ -44,12 +45,31 @@ export default function Run() {
       payload: { key, value },
     });
   };
+  const suite = query.data?.suite;
+  const branch = query.data?.branch;
   const statuses = ["pass", "fail", "dead", "running", "waiting"];
+  const date = query.data?.scheduled
+    ? format(new Date(query.data.scheduled), "yyyy-MM-dd")
+    : null;
   return (
     <div className={classes.root}>
       <Typography variant="h5" style={{ margin: "20px 0px" }}>
         {name}
       </Typography>
+      <div style={{ margin: "20px 0px" }}>
+        <Typography>See similar runs:</Typography>
+        <Link to={`/runs/?suite=${suite}&branch=${branch}`}>
+          <Typography>
+            suite {suite} and branch {branch}
+          </Typography>
+        </Link>
+        <Link to={`/runs/?branch=${branch}`}>
+          <Typography>branch {branch}</Typography>
+        </Link>
+        <Link to={`/runs/?date=${date}`}>
+          <Typography>scheduled on {date}</Typography>
+        </Link>
+      </div>
       <ButtonGroup style={{ display: "flex", justifyContent: "center" }}>
         <Button
           onClick={() => {
@@ -60,6 +80,7 @@ export default function Run() {
         </Button>
         {statuses.map((item) => (
           <Button
+            key={item}
             onClick={() => {
               setFilter("status", item);
             }}
