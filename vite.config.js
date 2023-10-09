5008d23c65e4b2da8128e6aa2077befcf0d98475
Zack Cerza <zack@redhat.com>
2023-10-09 11:29:25 -0600

    sentry: Enable source map uploads

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
+      sourcemap: true,  // for Sentry
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      // Sentry vite plugin goes after all other plugins
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "ceph",
+        project: "pulpito-ng",
+        url: "https://sentry.ceph.com/",
+      }),
+    ],
     server: {
       port: 8081,
     },
